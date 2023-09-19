35ab34634de0c327566bdbb6c2745a0e2ddacb12
Dario Reis <guttyerresjunior1995@gmail.com>
2023-09-19 13:16:53 -0300

    feat: config project api

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const cors = require('cors');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.listen(3001);
+console.log("api iniciada na porta 3001");
