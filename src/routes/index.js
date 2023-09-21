eda02ddb184f63c8d38edcc7c3892374b065709c
Dario Reis <guttyerresjunior1995@gmail.com>
2023-09-20 23:01:27 -0300

    feat: deploy

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,5 @@
 const { toolRoutes } = require("./tool.routes");
-const { userRoutes } = require("./user.routes");
 
 module.exports = (app) => {
   toolRoutes(app);
-  userRoutes(app);
 }
--- src/routes/user.routes.js
+++
@@ -1,9 +0,0 @@
-const { create, get, getId, update, remove } = require("../controllers/user.controller");
-
-exports.userRoutes = (app) => {
-  app.post("/user", create);
-  app.get("/user", get);
-  app.get("/user/:id", getId);
-  app.put("/user/:id", update);
-  app.delete("/user/:id", remove);
-}
